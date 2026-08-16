eb1c1dd06099c8e8c35d04344bd96802273dbfbd
hephaisto <1156478+hephaisto@users.noreply.github.com>
2026-08-16 15:45:05 +0200

    update generate_capacitor_radial_tht.py

--- generate_capacitor_radial_tht.py
+++ generate_capacitor_radial_tht.py
@@ -4,11 +4,10 @@ Generate THT polarized radial electrolytic capacitors (CAPPRD).
 
 import sys
 from os import path
-from uuid import uuid4
 
 from typing import Any, Optional
 
-from common import format_ipc_dimension, init_cache, now, save_cache
+from common import UuidCache, format_ipc_dimension, now
 from entities.common import (
     Align,
     Angle,
@@ -76,9 +75,7 @@ LEAD_WIDTH_TO_DRILL = {
     0.8: 1.0,
 }
 
-# Initialize UUID cache
-uuid_cache_file = 'uuid_cache_capacitors_radial_tht.csv'
-uuid_cache = init_cache(uuid_cache_file)
+uuid_cache = UuidCache('uuid_cache_capacitors_radial_tht.csv')
 
 
 def uuid(category: str, full_name: str, identifier: str) -> str:
@@ -93,10 +90,7 @@ def uuid(category: str, full_name: str, identifier: str) -> str:
         identifier:
             For example 'pad-1' or 'pin-13'.
     """
-    key = '{}-{}-{}'.format(category, full_name, identifier).lower().replace(' ', '~')
-    if key not in uuid_cache:
-        uuid_cache[key] = str(uuid4())
-    return uuid_cache[key]
+    return uuid_cache.get(category, full_name, identifier)
 
 
 def get_variant(
@@ -500,7 +494,7 @@ def generate_dev(
     print('Wrote device {}'.format(name))
 
 
-if __name__ == '__main__':
+def main() -> None:
     if '--help' in sys.argv or '-h' in sys.argv:
         print(f'Usage: {sys.argv[0]} [--3d]')
         print()
@@ -564,4 +558,7 @@ if __name__ == '__main__':
             create_date='2019-12-29T14:14:11Z',
         )
 
-    save_cache(uuid_cache_file, uuid_cache)
+
+if __name__ == '__main__':
+    with uuid_cache:
+        main()
