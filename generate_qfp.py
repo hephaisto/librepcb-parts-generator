cf317ffbfafafa74d0fe77f39abfe59b801aae59
hephaisto <1156478+hephaisto@users.noreply.github.com>
2026-08-30 14:12:01 +0200

    Directly call cache in generate_qfp.py

--- generate_qfp.py
+++ generate_qfp.py
@@ -333,21 +333,6 @@ OTHER_CONFIGS = [
 # fmt: on
 
 
-def uuid(category: str, full_name: str, identifier: str) -> str:
-    """
-    Return a uuid for the specified pin.
-
-    Params:
-        category:
-            For example 'cmp' or 'pkg'.
-        full_name:
-            For example "SOIC127P762X120-16".
-        identifier:
-            For example 'pad-1' or 'pin-13'.
-    """
-    return uuid_cache.get(category, full_name, identifier)
-
-
 class Pad:
     def __init__(self, x: float, y: float, orientation: str):
         self.x = x
@@ -434,7 +419,7 @@ def generate_pkg(
         full_description = config.description()
 
         def _uuid(identifier: str) -> str:
-            return uuid(category, full_name, identifier)
+            return uuid_cache.get(category, full_name, identifier)
 
         uuid_pkg = _uuid('pkg')
         uuid_pads = [_uuid('pad-{}'.format(p)) for p in range(1, config.lead_count + 1)]
@@ -796,7 +781,7 @@ def generate_pkg(
         add_footprint_variant('density~c', 'Density Level C (min protrusion)', 'C')
 
         # Generate 3D models
-        uuid_3d = uuid('pkg', full_name, '3d')
+        uuid_3d = uuid_cache.get('pkg', full_name, '3d')
         if generate_3d_models:
             generate_3d(library, full_name, uuid_pkg, uuid_3d, config)
         package.add_3d_model(Package3DModel(uuid_3d, Name(full_name)))
