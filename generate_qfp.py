b34496d7ed5d8deb21694000b97e5ea2adbef79c
hephaisto <1156478+hephaisto@users.noreply.github.com>
2026-08-30 12:56:35 +0200

    Update generate_qfp.py

--- generate_qfp.py
+++ generate_qfp.py
@@ -14,12 +14,11 @@ from collections import namedtuple
 from copy import deepcopy
 from itertools import chain
 from os import path
-from uuid import uuid4
 
 from typing import Dict, Iterable, List, Optional, cast
 
+from common import UuidCache, now, sign
 from common import format_ipc_dimension as fd
-from common import init_cache, now, save_cache, sign
 from entities.common import (
     Align,
     Angle,
@@ -87,9 +86,7 @@ silkscreen_offset = 0.150  # 150 µm
 courtyard_around_pads = 0.1  # 100 µm
 
 
-# Initialize UUID cache
-uuid_cache_file = 'uuid_cache_qfp.csv'
-uuid_cache = init_cache(uuid_cache_file)
+uuid_cache = UuidCache('uuid_cache_qfp.csv')
 
 
 # Based on Footprint Expert Guidelines, Chapter 7.0 (Nominal Calculation)
@@ -348,10 +345,7 @@ def uuid(category: str, full_name: str, identifier: str) -> str:
         identifier:
             For example 'pad-1' or 'pin-13'.
     """
-    key = '{}-{}-{}'.format(category, full_name, identifier).lower().replace(' ', '~')
-    if key not in uuid_cache:
-        uuid_cache[key] = str(uuid4())
-    return uuid_cache[key]
+    return uuid_cache.get(category, full_name, identifier)
 
 
 class Pad:
@@ -906,7 +900,7 @@ def generate_3d(
     assembly.save(out_path, fused=False)
 
 
-if __name__ == '__main__':
+def main() -> None:
     if '--help' in sys.argv or '-h' in sys.argv:
         print(f'Usage: {sys.argv[0]} [--3d]')
         print()
@@ -930,4 +924,8 @@ if __name__ == '__main__':
         version='0.5',
         create_date='2019-02-07T21:03:03Z',
     )
-    save_cache(uuid_cache_file, uuid_cache)
+
+
+if __name__ == '__main__':
+    with uuid_cache:
+        main()
