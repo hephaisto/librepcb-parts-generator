6c93a646cdee42ed4922b65a521fa1006d77a1e2
hephaisto <1156478+hephaisto@users.noreply.github.com>
2026-08-30 12:55:57 +0200

    Update generate_dpak.py

--- generate_dpak.py
+++ generate_dpak.py
@@ -7,12 +7,11 @@ import sys
 from collections import namedtuple
 from dataclasses import dataclass
 from os import path
-from uuid import uuid4
 
 from typing import Dict, Iterable, List, Optional, cast
 
+from common import UuidCache, now
 from common import format_ipc_dimension as fd
-from common import init_cache, now, save_cache
 from entities.common import (
     Align,
     Angle,
@@ -118,9 +117,7 @@ TAB_DENSITY_LEVELS: Dict[str, Excess] = {
 }
 
 
-# Initialize UUID cache
-uuid_cache_file = 'uuid_cache_dpak.csv'
-uuid_cache = init_cache(uuid_cache_file)
+uuid_cache = UuidCache('uuid_cache_dpak.csv')
 
 
 def uuid(category: str, full_name: str, identifier: str) -> str:
@@ -135,10 +132,7 @@ def uuid(category: str, full_name: str, identifier: str) -> str:
         identifier:
             For example 'pad-1' or 'pin-13'.
     """
-    key = '{}-{}-{}'.format(category, full_name, identifier).lower().replace(' ', '~')
-    if key not in uuid_cache:
-        uuid_cache[key] = str(uuid4())
-    return uuid_cache[key]
+    return uuid_cache.get(category, full_name, identifier)
 
 
 def excess_by_density(pitch: float, level: str) -> Excess:
@@ -666,7 +660,7 @@ def generate_3d(
     assembly.save(out_path, fused=False)
 
 
-if __name__ == '__main__':
+def main() -> None:
     if '--help' in sys.argv or '-h' in sys.argv:
         print(f'Usage: {sys.argv[0]} [--3d]')
         print()
@@ -719,4 +713,8 @@ if __name__ == '__main__':
         version='0.1',
         create_date='2026-06-01T08:27:05Z',
     )
-    save_cache(uuid_cache_file, uuid_cache)
+
+
+if __name__ == '__main__':
+    with uuid_cache:
+        main()
